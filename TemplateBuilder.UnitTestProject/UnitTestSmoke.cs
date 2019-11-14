b8c96ee035900d64adff586d924dd4ad5356500e
Stewart Rogers <Stewart.Rogers@celltrust.com>
2019-11-13 20:49:33 -0700

    Added Projects

---
+++ TemplateBuilder.UnitTestProject/UnitTestSmoke.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Text {
+    [TestClass]
+    public class UnitTestSmoke {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
