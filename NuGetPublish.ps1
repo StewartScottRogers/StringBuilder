e87e2f99eaf7e65926bbe7799ac91a012b4a61df
Stewart Rogers <Stewart.Rogers@celltrust.com>
2019-11-17 23:57:32 -0700

    Rapid Dev.

--- NuGetPublish.ps1
+++ NuGetPublish.ps1
@@ -1,3 +1,3 @@
 $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 Write-Host "Current script directory is $ScriptDir"
-dotnet nuget push $ScriptDir\TemplateBuilder\bin\Release\TemplateBuilder.1.0.1.nupkg -k oy2etn3zxhpc5ic6pzeaov2griugaotexhqqsjqckkk5ya -s https://api.nuget.org/v3/index.json
+dotnet nuget push $ScriptDir\TemplateBuilder\bin\Release\StringBuilder.ToTemplateBuilder.1.0.1.nupkg -k oy2etn3zxhpc5ic6pzeaov2griugaotexhqqsjqckkk5ya -s https://api.nuget.org/v3/index.json
